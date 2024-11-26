f7d4ea8f27b895be5ac2021a4214bb9993c72b6b
JotaErrE <jruiz967@gmail.com>
2024-11-25 23:13:27 -0500

    feat: socket connection

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,13 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { useSocket } from "@/hooks/useSocket";
+import { useEffect } from "react";
 
 
 export const ChatPage = () => {
-  const { online } = useSocket({
-    url: 'http://localhost:3001/socket.io/socket.io.js',
+  const { online, emit, on } = useSocket({
+    url: 'http://localhost:3001/',
     // options: {
     //   transports: ['websocket'],
     // },
   });
+
+  useEffect(() => {    
+    on('receiveMessage', (data: string) => {
+      console.log('Recibido:', data);
+    });
+
+    // eslint-disable-next-line
+  }, []);
 
   console.log({ online });
 
@@ -15,6 +25,12 @@ export const ChatPage = () => {
   return (
     <div>
       <h1>ChatPage</h1>
+
+      <Button 
+        onClick={() => {
+          emit('sendMessage', 'Hello from client');
+        }}
+      >Send</Button>
     </div>
   )
 };
